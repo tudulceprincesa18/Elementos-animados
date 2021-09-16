7487d7fdc1e901e16e29dd223de5f5f223adab43
Yasmin Martins <87744245+tudulceprincesa18@users.noreply.github.com>
2021-09-15 21:48:08 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+let menu = document.querySelector("#menuuno");
+let icono = document.querySelector("#menuicono");
+
+icono.addEventListener("click", function() {
+   menu.classList.toggle("menu-on");
+});
